2dbcb1be7545ed44a3f248e88ed9b0d145969bf5
gatoms <colpe.santi@gmail.com>
2021-09-17 10:13:09 -0300

    Carpinchos 6.5
    Hecho el test, no arregle lo otro.

--- js/test de carpinchos.js
+++ js/test de carpinchos.js
@@ -3,6 +3,7 @@
 var pregunta1 = "0"; 
 var pregunta2 = "0"; 
 var pregunta3 = "0";
+const test = [];
 function respuesta1(valor) 
 {pregunta1 = valor}; 
 function respuesta2(valor) 
@@ -10,6 +11,33 @@ function respuesta2(valor)
 function respuesta3(valor) 
 {pregunta3 = valor}; 
 
+function guardarTest(puntaje){
+    test.push(puntaje);
+    let a = JSON.stringify(test);
+    localStorage.setItem('puntajesTest', a);
+}
+
+function stars(xd){
+    document.getElementById('promedioTest').innerHTML = ''
+    for (let i = 0; i < xd; i++) {
+        document.getElementById('promedioTest').innerHTML += '<span class="fa fa-star checked">★</span>';
+    }
+    for (let i = 0; i < (5-xd); i++) {
+        document.getElementById('promedioTest').innerHTML += '<span class="fa fa-star">★</span>'
+    }
+}
+
+function promedioTest(){
+    let promedio = 0;
+    let total = 0;
+    for (let i = 0; i < test.length; i++) {
+        const element = test[i];
+        total += element; 
+    };
+    promedio = Math.round(total/test.length);
+    stars(promedio);
+    document.getElementById('cantidadTest').innerHTML = test.length;
+}
 
 function testChequeo(){
     puntuacion=0; 
@@ -55,26 +83,45 @@ function testChequeo(){
     if(indiceacierto==100) 
     {
     conteo(10)
+    guardarTest(5)
     mensaje="SOBRESALIENTE. Bien cabeza"} 
     if(indiceacierto<100&&indiceacierto>=75) 
     {
     conteo(2)
+    guardarTest(4)
     mensaje="NOTABLE. very good"} 
     if(indiceacierto<75&&indiceacierto>=50)
     {
     conteo(1)
+    guardarTest(3)
     mensaje="BIEN. okay"} 
     if(indiceacierto<50&&indiceacierto>=25)
     {
     conteo(0)
+    guardarTest(2)
     mensaje="JUSTILLO. vamo arriba"} 
     if(indiceacierto<25)
     {
     conteoNeg()
+    guardarTest(1)
     mensaje="DESASTROSO. bruh"} 
     if(contestadas==0)
     {
     mensaje="Contesta las preguntas gato"} 
 
+
+    promedioTest()
     alert("Puntaje: "+puntuacion+"." +  "\n\nEl numero maximo de puntos que podia conseguir era de " + puntosmaximos + ".\n\nHa dejado sin contestar "+ nocontesta+".\n\nSu porcentaje de aciertos es de "+indiceacierto+"%.\n\n"+mensaje+". ")
-}
+};
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    let a = localStorage.getItem('puntajesTest');
+    b = JSON.parse(a);
+    for (let i = 0; i < b.length; i++) {
+        test.push(b[i]);
+    };
+    promedioTest()
+});
+
+
